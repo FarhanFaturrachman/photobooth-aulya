ddcb3255a00b1c032a2ed9e71eb640e9a1086083
FarhanFaturrachman <farhanfaturrachman11@gmail.com>
2026-06-13 21:10:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -118,9 +118,9 @@ function loadFramesToContainer(containerId, className, isSidebar = false) {
     container.innerHTML = '';
     
     // Sesuaikan angka 10 dengan jumlah total file frame yang kamu punya
-    for(let i=1; i<=7; i++) {
+    for(let i=1; i<=1; i++) {
         const img = document.createElement('img');
-        img.src = `frame/frame${i}.PNG`; 
+        img.src = `frame/frame${i}.png`; 
         img.className = className;
         img.onclick = () => { 
             currentSelectedFrameSrc = img.src; 
